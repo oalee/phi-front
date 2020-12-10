3c568c2611c32bf5556ec3835fed384c8d8561eb
Ali Rahimi <ilaimihar@gmail.com>
2020-12-10 12:38:13 +0330

    added login path

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,31 +2,31 @@ import Head from 'next/head'
 import {useEffect, useState} from 'react'
 import Router from 'next/router'
 
- function Home(props) {
+ function LoginPage(props) {
 
-  const [user, setUser] = useState(props.user)
-  const [authorized, setAuthorized] = useState(false)
+  // const [user, setUser] = useState(props.user)
+  // const [authorized, setAuthorized] = useState(false)
 
-  useEffect ( (e)=>{
+  // useEffect ( (e)=>{
 
-    if(!authorized) {
-      Router.push('/login')
-    }
+  //   if(!authorized) {
+  //     Router.push('/login')
+  //   }
 
 
-  }, [])
+  // }, [])
 
 
   return (
     <div >
       <Head>
-        <title>Create Next App</title>
+        <title>Login</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className="h-screen w-screen bg-blue-500 justify-center relative " >
         <h1 className=" absolute  bottom-1/2 left-1/3 box-border rounded   w-1/3  text-white  py-8  text-3xl justify-center  text-bold bg-yellow-500 text-center">
-          Welcome to the party
+          Welcome to the login
         </h1>
        
       </div>
@@ -45,4 +45,4 @@ import Router from 'next/router'
 //   return { props: { data } }
 // }
 
-export default Home
+export default LoginPage
